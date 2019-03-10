8b5ad9fcbe1f62f413a799228d02b42e0a1669a5
HARRY BHATT <bob@brahmingate.co.uk>
2019-03-10 20:38:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
       resp.write handle_search(search_term)
       
     elsif req.path.match(/cart/)
-        if @@cart == []
+        if @@cart.length >= 1
             @@cart.each do |item|
                 resp.write "#{item}\n"
             end
